7345a14bfd7c23e5762c05dc27fabf6108f5fbae
DishonJP <dishonjp11@gmail.com>
2020-05-12 17:45:44 +0530

    [Dishon].add leapyear

---
+++ basic/leapYear/leapYear/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace leapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                Console.WriteLine("Enter a number to find leap year or not");
+                int year = Convert.ToInt32(Console.ReadLine());
+                string message;
+                if (year.ToString().Length == 4)
+                {
+                    message = year % 400 == 0 || year % 4 == 0 && year % 100 != 0 ? "Leap year" : "not a leap year";
+                }
+                else
+                {
+                    message = "year should be greater than 1000";
+                }
+                Console.WriteLine(message);
+            }
+			catch (Exception)
+			{
+                Console.WriteLine("should need to be number");
+			}
+        }
+    }
+}
