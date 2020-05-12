0a81205ea3680134ca15969453ae7853b876b70a
DishonJP <dishonjp11@gmail.com>
2020-05-12 18:06:52 +0530

    [Dishon].add powerof2

---
+++ basic/powerOf2/powerOf2/FindPower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace powerOf2
+{
+    class FindPower
+    {
+        public static int powerOf2(int value)
+        {
+            int finalValue = 1;
+            for (int i = 1; i <=value; i++)
+            {
+                finalValue *= 2;
+            }
+            return finalValue;
+        }
+    }
+
+}
