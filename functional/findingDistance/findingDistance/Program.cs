055ae09ce663946d208df710b1dee74a5b77daac
DishonJP <dishonjp11@gmail.com>
2020-05-13 11:30:28 +0530

    [Dishon].add distance to functional program

---
+++ functional/findingDistance/findingDistance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace functionalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            switch (Console.ReadLine())
+            {
+                case "distance":
+                    Console.WriteLine("Enter two points x and y");
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    double y = Convert.ToDouble(Console.ReadLine());
+                    double distance = findingDistance.findDistance(x, y);
+                    Console.WriteLine(distance.ToString());
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
---
+++ functional/findingDistance/findingDistance/findingDistance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace functionalPrograms
+{
+    class findingDistance
+    {
+        public static double findDistance(double x, double y)
+        {
+            return Math.Sqrt(Math.Pow(x, x) + Math.Pow(y, y));
+        }
+    }
+}
