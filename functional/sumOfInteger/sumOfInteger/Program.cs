4ea265a20a3c4d20e26e0c73d87f4f0674eeb5b0
DishonJP <dishonjp11@gmail.com>
2020-05-13 10:56:30 +0530

    [Dishon].add sun of integer to functional program

---
+++ functional/sumOfInteger/sumOfInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sumOfInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10] { 1, 2, 3, 4, 5, -2, 7, 8, -1, -5 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i]+array[j]==0)
+                    {
+                        Console.WriteLine("sun of {0} + {1} are 0", array[i] , array[j]);
+                    }
+                }
+            }
+        }
+    }
+}
